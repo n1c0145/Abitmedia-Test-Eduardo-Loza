e1f5530b8165c51d03760144823be307fbe340f0
n1c0145 <nicolas.lozalml@gmail.com>
2022-09-21 14:54:23 -0500

    Install material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+import {MatButtonModule} from '@angular/material/button';
+
+//Components
+import { NavComponent } from './components/nav/nav.component';
 import { LoginComponent } from './components/login/login.component';
 import { PaymentsIndexComponent } from './components/payments-index/payments-index.component';
 import { PaymentsCreatePaymentRequestComponent } from './components/payments-create-payment-request/payments-create-payment-request.component';
 import { PaymentsStatusTransactionComponent } from './components/payments-status-transaction/payments-status-transaction.component';
 import { PaymentsReverseComponent } from './components/payments-reverse/payments-reverse.component';
 import { InicioComponent } from './components/inicio/inicio.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ToastrModule } from 'ngx-toastr';
-import { NavComponent } from './components/nav/nav.component';
+
 
 @NgModule({
   declarations: [
@@ -33,6 +37,7 @@ import { NavComponent } from './components/nav/nav.component';
     HttpClientModule,
     BrowserAnimationsModule, 
     ToastrModule.forRoot(), 
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
