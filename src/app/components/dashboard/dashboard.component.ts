9f68978b70cdddd0f97a45b34daee342ffbcaaf2
n1c0145 <nicolas.lozalml@gmail.com>
2022-09-23 16:08:24 -0500

    Prod changes

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -10,6 +10,13 @@ export class DashboardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if (!localStorage.getItem('load')) { 
+      localStorage.setItem('load', 'no reload') 
+      setTimeout( function() { location.reload() ; }, 1000);
+      
+    } else {
+      localStorage.removeItem('load') 
+    }
   }
 
 }
